fd70b2fdeadc48bd3a919eed9b6fa637d2de4c2d
tumith <tumithorberg@gmail.com>
2020-12-03 12:16:02 +0000

    bara search eftir

--- lokaverk.py
+++ lokaverk.py
@@ -5,54 +5,63 @@ class Node:
         self.nxt = None
         self.head = None
         self.prv = None
-        self.rear = None
+        # self.rear = None sé ekki tilgang með þessum
     
     def insert(self,numer):
-        if self.nxt is None:
-            n  = Node(self.value)
-            n.value = numer
-            n.nxt = None
+        if self.nxt: # ef það er til næsti fer ég þangað...
+            return self.nxt.insert(numer)
+        else:
+            n = Node(numer)
+            # n.value = numer  # þetta er gert í constructor
+            self.nxt = n # nxt á self node bendir á nýjan node sem er aftast
+            n.prv = self #  prv á nýja hnútnum bendir á mig sjálfan
+            return True
 
+            """  sé ekki tilgang með þessu eða hvað ??????
             if self.head == None:
                 self.head = self.rear = n
             else:
                 self.rear.nxt = n
                 self.rear = n
-            
-        else:
-            return self.nxt.insert(self.value)
-    
+            """
+        
+            return self.nxt.insert(numer)
+
     def print_all(self):
-        if self.head is None:
+        if self is None:
             print('list is empty')
             return
         else:
-            n = Node(self.value)
-            print('List: ')
+            n = self
+            print('\n List: ', end="")
 
             while n != None:
-                print(n.value)
+                print(n.value,end="")
                 if n.nxt != None:
-                    print('-->')
+                    print(' --> ',end="")
 
                 n = n.nxt
-            print(self.value)
+            #print(self.value)
         
-    def search(self,key):
-        current = self.head
+    """def search(self,key):
+        tmp = self.value
+        mein_value = key
 
-        self.prv = None
+        #skoða hvort það er eitthvað í litanum eða hvort það er bara eins tala
+        if not tmp or not tmp.nxt:
+            return print('ekkert til að færa')
+        
 
-        while current != None:
-            if current.value == key:
-                if self.prv != None:
-                    self.prv.nxt = current.nxt
-                    current.nxt = self.head
-                    self.head = current
-                return True
-            self.prv = current
-            current = current.nxt
-        return False
+        while tmp and tmp.nxt:
+            mein_value = tmp
+            tmp = tmp.nxt
+        
+        # benda næst seinasta sem none
+        mein_value.nxt = None
+
+        # setja valda node sem firsta
+        tmp.nxt = self.value
+        self.value = tmp"""
 
 class Self_Orginizing:
     def __init__(self):
@@ -70,16 +79,26 @@ class Self_Orginizing:
         else:
             self.head.insert(value)
     
-    def search(self, value):
-        if self.head is None:
-            print('Það er ekkert í listanum')
-            return False
-        else:
-            return self.head.search(value)
+    def search(self, key):
+        tmp = self.head
+        mein_value = key
+
+        #skoða hvort það er eitthvað í litanum eða hvort það er bara eins tala
+        if not tmp or not tmp.nxt:
+            return print('ekkert til að færa')
+        
+        # benda næst seinasta sem none
+        mein_value.nxt = None
+
+        # setja valda node sem firsta
+        tmp.nxt = self.head
+        self.head = tmp
 
 SO = Self_Orginizing()
+
 SO.insert(5)           # 5
 SO.insert(7)           # 5 7
 SO.insert(6)           # 5 7 6
 SO.print_all()
-SO.search(6)           # 6 5 7
+SO.search(7)           # 6 5 7
+SO.print_all()
